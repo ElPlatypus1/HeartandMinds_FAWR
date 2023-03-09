7108686469a5a77be97d635e5e447f503ffbbbb3
TimRuskof <walarn33@gmail.com>
2023-03-09 15:15:04 +0100

    Update param.hpp

--- PERSISTANT_FAWR.sehreno/core/def/param.hpp
+++ PERSISTANT_FAWR.sehreno/core/def/param.hpp
@@ -441,7 +441,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_ARSENAL_GARAGEADMIN"]);
         values[]={0,1};
         texts[]={$STR_DISABLED,$STR_ENABLED};
-        default = 1;
+        default = 0;
     };
     class btc_p_main_title { // << Other options >>
         title = $STR_BTC_HAM_PARAM_OTHER_TITLE;
